e0785c048cfe7e7d1fa1bf32469847057b6cb800
Luis Tovar <130521074+LTovarAlc@users.noreply.github.com>
2023-11-29 10:59:23 -0600

    banner ok

--- src/Components/BannerRegister/bannerRegister.jsx
+++ src/Components/BannerRegister/bannerRegister.jsx
@@ -3,7 +3,14 @@ import React from "react"
 
 const BannerRegister = () => {
     return<div className="banner">
-        
+        <div className="logo__container">
+            <img src="../../img/logoBanner.png" alt="" className="logoBanner" />
+        </div>
+        <div className="presentation__banner">
+            <h1 className="text__banner">
+                G-SHOP <br /> TE DA LA <br /> BIENVENIDA!
+            </h1>
+        </div>
     </div>
 }
 
