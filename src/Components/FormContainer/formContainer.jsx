1f7217d43b82cc6c258d1b6c3287abd28d384225
Luis Tovar <130521074+LTovarAlc@users.noreply.github.com>
2023-12-05 10:49:28 -0600

    form register & form login visibles

--- src/Components/FormContainer/formContainer.jsx
+++ src/Components/FormContainer/formContainer.jsx
@@ -1,61 +1,24 @@
 import "./formContainer.css";
 import "animate.css";
-import React from "react";
+import React, { useState } from "react";
+import FormRegister from "../FormRegister/formRegister";
+import FormLogin from "../FormLogin/formLogin";
 
 const FormContainer = () => {
+  const [showRegister, setShowRegister] = useState(true);
+
+  const toggleForm = () => {
+    setShowRegister((prevShowRegister) => !prevShowRegister);
+  };
+
   return (
     <section className="section__form">
       <div className="form__container animate__animated animate__bounceInRight animate__delay-1s">
-        <h2 className="form__title">Registrarse</h2>
-        <form action="" className="formRegister">
-          <div className="input__container-forms">
-            <input
-              type="text"
-              name="name"
-              id="name"
-              required
-              placeholder="Nombre..."
-            />
-            <input
-              type="text"
-              name="lastName"
-              id="lastName"
-              required
-              placeholder="Apelldos..."
-            />
-          </div>
-          <div className="input__container-forms">
-            <input
-              type="text"
-              name="email"
-              id="email"
-              required
-              placeholder="Correo electronico..."
-            />
-          </div>
-          <div className="input__container-forms">
-            <input
-              type="password"
-              name="password"
-              id="password"
-              placeholder="Contraseña"
-              required
-              pattern="^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,14}$"
-            />
-            <input
-              type="password"
-              name="confirmPassword"
-              id="confirmPassword"
-              placeholder="Confirmar contraseña"
-              required
-            />
-          </div>
-        </form>
-        <div className="btnSubmit__container">
-          <button type="submit" className="btn__submit">Registrarse</button>
-        </div>
+        {showRegister ? <FormRegister /> : <FormLogin />}
         <div className="IGAC__container">
-          <span className="IGAC">Ya tengo una cuenta</span>
+          <span className="IGAC" onClick={toggleForm}>
+            {showRegister ? "Ya tengo una cuenta" : "Crear cuenta"}
+          </span>
         </div>
       </div>
     </section>
--- src/Components/FormRegister/formRegister.jsx
+++ src/Components/FormRegister/formRegister.jsx
@@ -1,10 +1,25 @@
-import "./formLogin.css";
+import React from "react";
 
-const FormLogin = () => {
-  return (
-    <>
-        <h2 className="form__title">Iniciar Sesion</h2>
+const FormRegister = () => {
+    return <>
+    <h2 className="form__title">Registrarse</h2>
         <form action="" className="formRegister">
+          <div className="input__container-forms">
+            <input
+              type="text"
+              name="name"
+              id="name"
+              required
+              placeholder="Nombre..."
+            />
+            <input
+              type="text"
+              name="lastName"
+              id="lastName"
+              required
+              placeholder="Apelldos..."
+            />
+          </div>
           <div className="input__container-forms">
             <input
               type="text"
@@ -33,12 +48,9 @@ const FormLogin = () => {
           </div>
         </form>
         <div className="btnSubmit__container">
-          <button type="submit" className="btn__submit">
-            Iniciar sesion
-          </button>
+          <button type="submit" className="btn__submit">Registrarse</button>
         </div>
     </>
-  );
-};
+}
 
-export default FormLogin;
+export default FormRegister
