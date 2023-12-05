c69cf1be4077584e9013f9924cf2981cbad8e70a
Luis Tovar <130521074+LTovarAlc@users.noreply.github.com>
2023-12-05 10:58:40 -0600

    show password ok

--- src/Components/FormLogin/formLogin.jsx
+++ src/Components/FormLogin/formLogin.jsx
@@ -1,42 +1,51 @@
-import "./formLogin.css";
+import "./formLogin.css"
+import React, { useState } from "react";
 
 const FormLogin = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+
   return (
     <>
-        <h2 className="form__title">Iniciar Sesion</h2>
-        <form action="" className="formRegister">
-          <div className="input__container-forms">
-            <input
-              type="text"
-              name="email"
-              id="email"
-              required
-              placeholder="Correo electronico..."
-            />
-          </div>
-          <div className="input__container-forms">
-            <input
-              type="password"
-              name="password"
-              id="password"
-              placeholder="Contraseña"
-              required
-              pattern="^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,14}$"
-            />
-            <input
-              type="password"
-              name="confirmPassword"
-              id="confirmPassword"
-              placeholder="Confirmar contraseña"
-              required
-            />
-          </div>
-        </form>
-        <div className="btnSubmit__container">
-          <button type="submit" className="btn__submit">
-            Iniciar sesion
-          </button>
+      <h2 className="form__title">Iniciar Sesion</h2>
+      <form action="" className="formRegister">
+        <div className="input__container-forms">
+          <input
+            type="text"
+            name="email"
+            id="email"
+            required
+            placeholder="Correo electronico"
+          />
         </div>
+        <div className="input__container-forms">
+          <input
+            type={showPassword ? "text" : "password"}
+            name="password"
+            id="password"
+            placeholder="Contraseña"
+            required
+          />
+          <input
+            type={showPassword ? "text" : "password"}
+            name="confirmPassword"
+            id="confirmPassword"
+            placeholder="Confirmar contraseña"
+            required
+          />
+          <span className="show__password" onClick={togglePasswordVisibility}>
+            {showPassword ? "Ocultar contraseña" : "Mostrar contraseña"}
+          </span>
+        </div>
+      </form>
+      <div className="btnSubmit__container">
+        <button type="submit" className="btn__submit">
+          Iniciar sesion
+        </button>
+      </div>
     </>
   );
 };
--- src/Components/FormRegister/formRegister.jsx
+++ src/Components/FormRegister/formRegister.jsx
@@ -1,56 +1,69 @@
 import React from "react";
+import { useState } from "react";
 
 const FormRegister = () => {
-    return <>
-    <h2 className="form__title">Registrarse</h2>
-        <form action="" className="formRegister">
-          <div className="input__container-forms">
-            <input
-              type="text"
-              name="name"
-              id="name"
-              required
-              placeholder="Nombre..."
-            />
-            <input
-              type="text"
-              name="lastName"
-              id="lastName"
-              required
-              placeholder="Apelldos..."
-            />
-          </div>
-          <div className="input__container-forms">
-            <input
-              type="text"
-              name="email"
-              id="email"
-              required
-              placeholder="Correo electronico..."
-            />
-          </div>
-          <div className="input__container-forms">
-            <input
-              type="password"
-              name="password"
-              id="password"
-              placeholder="Contraseña"
-              required
-              pattern="^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,14}$"
-            />
-            <input
-              type="password"
-              name="confirmPassword"
-              id="confirmPassword"
-              placeholder="Confirmar contraseña"
-              required
-            />
-          </div>
-        </form>
-        <div className="btnSubmit__container">
-          <button type="submit" className="btn__submit">Registrarse</button>
-        </div>
-    </>
-}
+  const [showPassword, setShowPassword] = useState(false);
 
-export default FormRegister
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+
+  return (
+    <>
+      <h2 className="form__title">Registrarse</h2>
+      <form action="" className="formRegister">
+        <div className="input__container-forms">
+          <input
+            type="text"
+            name="name"
+            id="name"
+            required
+            placeholder="Nombre..."
+          />
+          <input
+            type="text"
+            name="lastName"
+            id="lastName"
+            required
+            placeholder="Apelldos..."
+          />
+        </div>
+        <div className="input__container-forms">
+          <input
+            type="text"
+            name="email"
+            id="email"
+            required
+            placeholder="Correo electronico..."
+          />
+        </div>
+        <div className="input__container-forms">
+          <input
+            type={showPassword ? "text" : "password"}
+            name="password"
+            id="password"
+            placeholder="Contraseña"
+            required
+          />
+          <input
+            type={showPassword ? "text" : "password"}
+            name="confirmPassword"
+            id="confirmPassword"
+            placeholder="Confirmar contraseña"
+            required
+          />
+          <span className="show__password" onClick={togglePasswordVisibility}>
+            {showPassword ? "Ocultar contraseña" : "Mostrar contraseña"}
+          </span>
+        </div>
+      </form>
+      <div className="btnSubmit__container">
+        <button type="submit" className="btn__submit">
+          Registrarse
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default FormRegister;
